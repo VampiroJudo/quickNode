40f8c54253868a008268e18f434b9e6c7258bb76
John <johnconley507@hotmail.com>
2017-06-28 20:55:06 -0400

    creating and implementing maongodb database with mLab

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://JohnC:rednef1500@ds143362.mlab.com:43362/quicknode_john', ['tasks']);
 
 router.get('/tasks', function(req, res, next) {
-	res.send('TASK API');
+	db.tasks.find(function(err, tasks) {
+		if(err) {
+			res.send(err);
+		}
+		res.json(tasks);
+	})
 });
 
 module.exports = router;

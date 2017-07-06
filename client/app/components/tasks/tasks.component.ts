e2563d6b9dda12f5992111a5ce490bada44cea97
John <johnconley507@hotmail.com>
2017-07-05 21:42:53 -0400

    adding services to project

--- client/app/components/tasks/tasks.component.ts
+++ client/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular.core';
+import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 
 @Component({
 	moduleId: module.id,
@@ -6,4 +7,8 @@ import { Component } from '@angular.core';
 	templateUrl: 'tasks.component.html'
 })
 
-export class TasksComponent { }
+export class TasksComponent {
+	constructor(private taskService:TaskService) {
+
+	}
+}
